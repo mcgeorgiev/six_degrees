9c9043afd14eccd42d90f017472461a4ac3b0498
richardknox <1101997k@student.gla.ac.uk>
2017-02-28 12:18:35 +0000

    Wiki.py generates json for a given page (by title)

--- Wiki.py
+++ Wiki.py
@@ -1,12 +1,38 @@
 import requests
+import json
+from itertools import count
 
 # page id to get links currently all links from "broughty ferry" page
-page_id = 360393
+def get_page_links(name):
+    page_name = name
 
-api_url = "https://en.wikipedia.org/w/api.php?action=query&pageids=%s&prop=links&pllimit=max&format=json" % page_id
+    api_url = "https://en.wikipedia.org/w/api.php?action=query&titles={0}&prop=links&pllimit=10&format=json".format(page_name)
 
-r = requests.get(api_url)
-resp = r.json()
+    r = requests.get(api_url)
+    resp = r.json()
 
-for links in resp['query']['pages']['%d'%page_id]['links']:
-    print links['title']
+    #page_id = resp['query']['pages']['360393']['pageid']
+    for nested_json in resp["query"]["pages"]:
+        page_id = nested_json
+    print page_id
+    # build json response with all current nodes
+    data = {}
+    data["nodes"] = [{"id":"n0", "label":resp["query"]["pages"]["{0}".format(page_id)]["title"], "x":0, "y":0, "size":2}]
+    data["edges"] = []
+    counter = count(1)
+    for links in resp["query"]["pages"]["{0}".format(page_id)]["links"]:
+        # print links['title']
+        node_id = counter.next()
+        data["nodes"] += {"id":"n{0}".format(node_id),
+                        "label":links['title'],
+                        "size":1,
+                        "x":node_id,
+                        "y":node_id},
+        data["edges"] += {"id":"e{0}".format(node_id),
+                        "source":"n{0}".format((node_id-1)),
+                        "target":"n{0}".format(node_id)},
+
+    with open('sigma/data/test.json', 'w') as outfile:
+        json.dump(data, outfile, indent=2)
+if __name__ == '__main__':
+    get_page_links("Broughty Ferry")
