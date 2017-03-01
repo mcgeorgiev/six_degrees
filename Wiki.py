f59148cc7d53f7a27d79ae80a115dd1f8efbd01d
mcgeorgiev <michael_georgiev@hotmail.co.uk>
2017-03-01 15:21:26 +0000

    more neo4j functions

--- Wiki.py
+++ Wiki.py
@@ -6,7 +6,7 @@ from itertools import count
 def get_page_links(name):
     page_name = name
 
-    api_url = "https://en.wikipedia.org/w/api.php?action=query&titles={0}&prop=links&pllimit=10&format=json".format(page_name)
+    api_url = "https://en.wikipedia.org/w/api.php?action=query&titles={0}&prop=links&pllimit=5&format=json".format(page_name)
 
     r = requests.get(api_url)
     resp = r.json()
@@ -14,25 +14,33 @@ def get_page_links(name):
     #page_id = resp['query']['pages']['360393']['pageid']
     for nested_json in resp["query"]["pages"]:
         page_id = nested_json
-    print page_id
+    #print page_id
     # build json response with all current nodes
     data = {}
     data["nodes"] = [{"id":"n0", "label":resp["query"]["pages"]["{0}".format(page_id)]["title"], "x":0, "y":0, "size":2}]
     data["edges"] = []
     counter = count(1)
-    for links in resp["query"]["pages"]["{0}".format(page_id)]["links"]:
-        # print links['title']
-        node_id = counter.next()
-        data["nodes"] += {"id":"n{0}".format(node_id),
-                        "label":links['title'],
-                        "size":1,
-                        "x":node_id,
-                        "y":node_id},
-        data["edges"] += {"id":"e{0}".format(node_id),
-                        "source":"n{0}".format((node_id-1)),
-                        "target":"n{0}".format(node_id)},
 
-    with open('sigma/data/test.json', 'w') as outfile:
-        json.dump(data, outfile, indent=2)
+    all_links = []
+    for links in resp["query"]["pages"]["{0}".format(page_id)]["links"]:
+        all_links.append(links)
+    print all_links
+    return all_links
+
+
+    #     ### added this to check something
+    #     node_id = counter.next()
+    #     data["nodes"] += {"id":"n{0}".format(node_id),
+    #                     "label":links['title'],
+    #                     "size":1,
+    #                     "x":node_id,
+    #                     "y":node_id},
+    #     data["edges"] += {"id":"e{0}".format(node_id),
+    #                     "source":"n{0}".format((node_id-1)),
+    #                     "target":"n{0}".format(node_id)},
+    #
+    # with open('sigma/data/test.json', 'w') as outfile:
+    #     json.dump(data, outfile, indent=2)
+
 if __name__ == '__main__':
-    get_page_links("Broughty Ferry")
+    get_page_links("Scotland")
--- graph.py
+++ graph.py
@@ -1,5 +1,10 @@
 from neo4j.v1 import GraphDatabase, basic_auth
 
+def get_session():
+    driver = GraphDatabase.driver("bolt://localhost:7687", auth=basic_auth("neo4j", "password"))
+    return driver.session()
+
+
 def get_nodes_from_game(start, game, end):
     driver = GraphDatabase.driver("bolt://localhost:7687", auth=basic_auth("neo4j", "password"))
     session = driver.session()
@@ -33,12 +38,116 @@ def get_nodes_from_game(start, game, end):
                 elif entry["source"]["name"] == output["nodes"][i]["dest"]["name"]:
                     output["nodes"].insert(i+1, entry)
                     break
+    session.close()
     print output
     return output
 
-if __name__ == "__main__":
-    start = "scotland"
-    game = "game2"
-    end = "kangaroo"
 
-    get_nodes_from_game(start, game, end)
+def add_node(article, wiki_id):
+    driver = GraphDatabase.driver("bolt://localhost:7687", auth=basic_auth("neo4j", "password"))
+    session = driver.session()
+    # need to escape the curly braces
+    query = "CREATE ({0}:Article {{name:'{1}', id:{2}}})".format(format_string(article), article, wiki_id)
+    print query
+    session.run(query)
+    session.close()
+
+
+def create_session():
+    driver = GraphDatabase.driver("bolt://localhost:7687", auth=basic_auth("neo4j", "password"))
+    session =  driver.session()
+    print session
+    return session
+
+
+def add_edge(nodeA, nodeB, game_id):
+    driver = GraphDatabase.driver("bolt://localhost:7687", auth=basic_auth("neo4j", "password"))
+    session =  driver.session()
+
+    query = """MATCH (a:Article),(b:Article) WHERE a.name = '{0}' AND b.name = '{1}'
+               CREATE (a)-[r:{2}]->(b)""".format(nodeA["name"], nodeB["name"], game_id)
+    session.run(query)
+    session.close()
+
+
+def get_node_from_name(name):
+    driver = GraphDatabase.driver("bolt://localhost:7687", auth=basic_auth("neo4j", "password"))
+    session =  driver.session()
+
+    query = "match (node:Article{{name: '{0}'}}) return node".format(name)
+    results = session.run(query)
+    session.close()
+    for record in results:
+        return record['node'].properties
+
+
+def get_related_nodes(node):
+    driver = GraphDatabase.driver("bolt://localhost:7687", auth=basic_auth("neo4j", "password"))
+    session =  driver.session()
+
+    query = "MATCH (Article {{ name: '{0}' }})--(related) return related".format(node["name"])
+    results = session.run(query)
+    session.close()
+
+    all_nodes = []
+    for record in results:
+        all_nodes.append(record["related"].properties)
+    distinct_nodes = {value['id']:value for value in all_nodes}.values()
+    return distinct_nodes
+
+
+def has_enough_edges(current_node):
+    # if the node has one relationship then that is it's source
+    min_relations = 10
+    driver = GraphDatabase.driver("bolt://localhost:7687", auth=basic_auth("neo4j", "password"))
+    session =  driver.session()
+
+    query = "MATCH (Article {{ name: '{0}' }})--(related) return related.name".format(current_node["name"])
+    results = session.run(query)
+    session.close()
+
+    all_relations = []
+    for record in results:
+        all_relations.append(record["related.name"])
+    distinct_relations = frozenset(all_relations)
+
+    return True if len(distinct_relations) >= min_relations else False
+
+def player_selects_node():
+    current = {}
+    if has_enough_edges(current_node):
+        get_related_nodes(current_node)
+        # return the nodes to the front end
+    else:
+        pass
+
+def does_not_exist():
+    # make sure duplicates are not added
+    pass
+
+def add_API_nodes():
+    pass
+
+def format_string(string):
+    string = string.replace(" ", "_")
+    string = string.lower()
+    return string
+
+
+
+# * Useful code to find an objects properties names
+# for property, value in vars(related).iteritems():
+#     print property, ": ", value
+
+if __name__ == "__main__":
+    print get_related_nodes(get_node_from_name("Neolithic"))
+    #print has_enough_edges({"name": "Neolithic"})
+    #add_edge({"name": "England"}, {"name": "Scotland"}, "game3")
+    #add_node("England", 9)
+    #add_node("Anglican Communion", 8)
+
+    # start = "scotland"
+    # game = "game2"
+    # end = "kangaroo"
+    #
+    # get_nodes_from_game(start, game, end)
