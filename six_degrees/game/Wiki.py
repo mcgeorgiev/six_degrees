860fa2c08ebc7e14356e7b8c092ca2439667c1db
mcgeorgiev <michael_georgiev@hotmail.co.uk>
2017-03-02 15:20:33 +0000

    converts db response for sigma

--- six_degrees/game/Wiki.py
+++ six_degrees/game/Wiki.py
@@ -15,11 +15,12 @@ def get_page_links(name):
     #page_id = resp['query']['pages']['360393']['pageid']
     for nested_json in resp["query"]["pages"]:
         page_id = nested_json
-    #print page_id
+
     # build json response with all current nodes
     data = {}
     data["nodes"] = [{"id":"n0", "label":resp["query"]["pages"]["{0}".format(page_id)]["title"], "x":0, "y":0, "size":2}]
     data["edges"] = []
+
     counter = count(1)
 
     all_links = []
@@ -37,24 +38,12 @@ def get_page_links(name):
         #                 "target":"n{0}".format(node_id)},
 
         all_links.append(links)
-    # with open('sigma/data/test.json', 'w') as outfile:
-    #     json.dump(data, outfile, indent=2)
-    # print json.dumps(data, indent=2)
-    return json.dumps(all_links)
 
-    #     ### added this to check something
-    #     node_id = counter.next()
-    #     data["nodes"] += {"id":"n{0}".format(node_id),
-    #                     "label":links['title'],
-    #                     "size":1,
-    #                     "x":node_id,
-    #                     "y":node_id},
-    #     data["edges"] += {"id":"e{0}".format(node_id),
-    #                     "source":"n{0}".format((node_id-1)),
-    #                     "target":"n{0}".format(node_id)},
-    #
     # with open('sigma/data/test.json', 'w') as outfile:
     #     json.dump(data, outfile, indent=2)
+
+
+
 
 if __name__ == '__main__':
     get_page_links("Scotland")
--- six_degrees/game/views.py
+++ six_degrees/game/views.py
@@ -3,12 +3,14 @@ from django.http import HttpResponse
 from Wiki import get_page_links
 from django.views.decorators.clickjacking import xframe_options_sameorigin
 import json
+from graph import *
 
 def index(request):
     # Returns a rendered response to send to the client
     context_dict = {"something": "else"}
     response = render(request, 'game/index.html', context=context_dict)
     return response
+
 
 # incoming
 def incoming_node(request, title):
@@ -17,3 +19,37 @@ def incoming_node(request, title):
     print type(data_back)
 
     return HttpResponse(data_back)
+
+
+def incoming_node(request, name):
+    current_node = {"name": name, "id": 0}
+
+    if has_enough_edges(current_node):
+        db_nodes = get_related_nodes(current_node)
+    else:
+        db_nodes = add_API_nodes(current_node)
+
+    return HttpResponse(convert_for_sigma(current_node, db_nodes))
+
+
+def convert_for_sigma(current_node, all_nodes):
+    # build json response with all current nodes
+    data = {}
+    data["nodes"] = [{"id":"n0", "label":current_node["name"], "x":0, "y":0, "size":2}]
+    data["edges"] = []
+
+    counter = count(1)
+
+    all_links = []
+    for links in all_nodes:
+        node_id = counter.next()
+        data["nodes"] += {"id":"n{0}".format(node_id),
+                        "label":links['name'],
+                        "size":1,
+                        "x":node_id,
+                        "y":node_id},
+        # and edges between nodes
+        data["edges"] += {"id":"e{0}".format(node_id),
+                        "source":"n0",
+                        "target":"n{0}".format(node_id)},
+    return data
