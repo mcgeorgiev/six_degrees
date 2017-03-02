bdbb9457f0fef3e1c798b8a34b00fa3b670dfa2a
mcgeorgiev <michael_georgiev@hotmail.co.uk>
2017-03-02 12:20:10 +0000

    fixed

--- six_degrees/game/views.py
+++ six_degrees/game/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from
 
 def index(request):
     # Returns a rendered response to send to the client
@@ -11,14 +10,3 @@ def index(request):
 # incoming
 def incoming_node(request, title):
     return HttpResponse(title + "11111")
-
-
-def player_selects_node(request):
-
-
-    if has_enough_edges(current_node):
-        get_related_nodes(current_node)
-        # return the nodes to the front end
-    else:
-        add_API_nodes(current_node)
-        # return nodes to the frontends
