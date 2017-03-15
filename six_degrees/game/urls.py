d311f708562d36621a978f1fb9ee14006c0a0eaf
richardknox <1101997k@student.gla.ac.uk>
2017-03-15 13:04:28 +0000

    dashboard, some changes to views etc

--- six_degrees/game/urls.py
+++ six_degrees/game/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
 	url(r'^incomingnode/((?P<title>.*))/$', views.incoming_node, name='incoming_node'),
 	url(r'^start/$', views.get_start_node, name='get_start_node'),
 	url(r'^gameover/$', views.game_over, name='game_over'),
+	url(r'^dashboard/$', views.dashboard, name='dashboard')
 ]
